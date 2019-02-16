80496ec96bbf29573f9268ffc2d08a6811d15810
mishu <mishu@xtelecom.ro>
2019-02-16 17:35:00 +0200

    fixes import / export & dependency

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ import { Modal, ModalCard } from '@enso-ui/modal/bulma';
 import ProgressBar from '@enso-ui/progress-bar';
 import { Tab, Tabs, EnsoTabs } from '@enso-ui/tabs/bulma';
 import Toastr from '@enso-ui/toastr/bulma';
+import { Toastr as ToasterPlugin } from '@enso-ui/toastr';
 import Typeahead from '@enso-ui/typeahead/bulma';
 
 export {
     Modal, ModalCard, ProgressBar, Tab, Tabs,
-    EnsoTabs, Toastr, Typeahead,
+    EnsoTabs, Toastr, ToasterPlugin, Typeahead,
 };
