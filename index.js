683d3c28cd355dd9536975ffc04cafcdb9a87cb6
mishu <mishu@xtelecom.ro>
2019-02-17 11:06:53 +0200

    adds datepicker

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import Datepicker from '@enso-ui/datepicker/bulma';
 import { Modal, ModalCard } from '@enso-ui/modal/bulma';
 import ProgressBar from '@enso-ui/progress-bar';
 import { Tab, Tabs, EnsoTabs } from '@enso-ui/tabs/bulma';
@@ -6,6 +7,6 @@ import ToastrPlugin from '@enso-ui/toastr';
 import Typeahead from '@enso-ui/typeahead/bulma';
 
 export {
-    Modal, ModalCard, ProgressBar, Tab, Tabs,
+    Datepicker, Modal, ModalCard, ProgressBar, Tab, Tabs,
     EnsoTabs, Toastr, ToastrPlugin, Typeahead,
 };
