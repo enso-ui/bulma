5308d0da24f1e3510db769193b58ac6dc7af89f5
mishu <mishu@xtelecom.ro>
2019-02-18 08:38:34 +0200

    adds vue switch

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ import { Tab, Tabs, EnsoTabs } from '@enso-ui/tabs/bulma';
 import Toastr from '@enso-ui/toastr/bulma';
 import ToastrPlugin from '@enso-ui/toastr';
 import Typeahead from '@enso-ui/typeahead/bulma';
+import VueSwitch from '@enso-ui/switch/bulma';
 
 export {
     Datepicker, Modal, ModalCard, ProgressBar, Tab, Tabs,
-    EnsoTabs, Toastr, ToastrPlugin, Typeahead,
+    EnsoTabs, Toastr, ToastrPlugin, Typeahead, VueSwitch,
 };
