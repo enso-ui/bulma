fa723a898cf98141ac792abca084106313b1b300
mishu <mishu@xtelecom.ro>
2019-02-19 19:35:31 +0200

    adds cards, transitions

--- index.js
+++ index.js
@@ -1,22 +1,40 @@
+import {
+    Card, CardHeader, CardContent, CardImage, CardFooter,
+    CardFooterItem, CardControl, CardBadge, CardCollapse,
+    CardRefresh, CardRemove,
+} from '@enso-ui/card/bulma';
+
 import Datepicker from '@enso-ui/datepicker/bulma';
+
 import Loader from '@enso-ui/loader';
+
 import { Modal, ModalCard } from '@enso-ui/modal/bulma';
+
 import ProgressBar from '@enso-ui/progress-bar';
+
 import { Tab, Tabs, EnsoTabs } from '@enso-ui/tabs/bulma';
+
 import Toastr from '@enso-ui/toastr/bulma';
 import ToastrPlugin from '@enso-ui/toastr';
+
+import { Fade, Zoom } from '@enso-ui/transitions';
+
 import Typeahead from '@enso-ui/typeahead/bulma';
+
 import VueSwitch from '@enso-ui/switch/bulma';
+
 import {
     IntervalFilter, DateFilter, DateIntervalFilter, EnsoIntervalFilter,
     EnsoDateFilter, EnsoDateIntervalFilter, BooleanFilter, VueFilter,
     EnsoVueFilter,
 } from '@enso-ui/filters/bulma';
 
-
 export {
-    Datepicker, Loader, Modal, ModalCard, ProgressBar, Tab, Tabs, EnsoTabs,
-    Toastr, ToastrPlugin, Typeahead, VueSwitch, IntervalFilter, DateFilter,
-    DateIntervalFilter, EnsoIntervalFilter, EnsoDateFilter,
+    Card, CardHeader, CardContent, CardImage, CardFooter,
+    CardFooterItem, CardControl, CardBadge, CardCollapse,
+    CardRefresh, CardRemove, Datepicker, Loader, Modal,
+    ModalCard, ProgressBar, Tab, Tabs, EnsoTabs, Toastr,
+    ToastrPlugin, Fade, Zoom, Typeahead, VueSwitch, IntervalFilter,
+    DateFilter, DateIntervalFilter, EnsoIntervalFilter, EnsoDateFilter,
     EnsoDateIntervalFilter, BooleanFilter, VueFilter, EnsoVueFilter,
 };
